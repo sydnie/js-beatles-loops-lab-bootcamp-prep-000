143e43ad96bec35babb9dc38968595c6d57d1736
Sydnie T <sydnietom@gmail.com>
2018-04-03 05:51:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function theBeatlesPlay(musicians, instruments) {
+  
+  var emptyArray = [];
+  
+  for (i = 0, i < musicians.length, i++) {
+    emptyArray[`${musicians} plays ${instruments}`];
+    }
+
+  return array;
+}
