08a2f3bf6e947bf19f8ca3efa002daaf5e00e1fb
Sydnie T <sydnietom@gmail.com>
2018-04-03 06:06:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ function theBeatlesPlay(musicians, instruments) {
   
   var result = [];
   
-  for (var i = 0; i < musicians.length; i++) {
+  for (i = 0; i < musicians.length; i++) {
     result.push(`${musicians[i]} plays ${instruments[i]}`);
   }
 
   return result;
 }
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+  ];
+
 function johnLennonFacts(facts) {
   
+  for (i = 0; i < facts.length; i++) {
+    editedFacts.push(i + "!!!");
+  }
+  
+  return editedFacts
+  
 }
+
+
+
+
+
+
+
