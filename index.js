7252a5b09d4fdec91f9cd92760dbb93624a58c08
Sydnie T <sydnietom@gmail.com>
2018-04-03 06:33:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,8 +32,9 @@ function iLoveTheBeatles(number) {
   var results = [];
   
   do {
+    number++;
     results.push("I love the Beatles!");
-  } while (number++ < 15);
+  } while (number < 15);
   
   return results;
 }
